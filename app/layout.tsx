143d5b960cc4baac16479464a49113dd85a30d23
pjleb90 <pjleb90@gmail.com>
2023-06-04 10:33:13 -0400

    created ClientOnly file to stop next.js hydration error when reloading

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Nunito } from 'next/font/google'
 
 import './globals.css'
 import Navbar from './components/navbar/Navbar';
+import ClientOnly from './components/ClientOnly';
 
 const font = Nunito({
   subsets: ["latin"],
@@ -20,7 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
