c67e6cf346c166b255432bce4c07bc6f4a5df0d0
pjleb90 <pjleb90@gmail.com>
2023-06-08 18:10:31 -0400

    added Categories and CategoryBox components

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -4,6 +4,7 @@ import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
+import Categories from "./Categories";
 
 import { SafeUser } from "@/app/types";
 
@@ -33,6 +34,7 @@ const Navbar: React.FC<NavbarProps> = ({
                     </div>
                 </Container>
             </div>
+            <Categories />
         </div>
     )
 }
