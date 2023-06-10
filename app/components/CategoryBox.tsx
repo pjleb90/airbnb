ffa1d971f12a92eec24e8261fef4479c2faa557c
pjleb90 <pjleb90@gmail.com>
2023-06-10 08:39:50 -0400

    added url's and query logic for categories

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -1,4 +1,7 @@
+import { useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
 import { IconType } from "react-icons";
+import qs from "query-string"
 
 interface CategoryBoxProps {
     icon: IconType;
@@ -11,8 +14,42 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
     label,
     selected
 }) => {
+    const router = useRouter();
+    const params = useSearchParams();
+
+    const handleClick = useCallback(() => {
+        let currentQuery = {};
+
+        // define empty query
+        if (params) {
+            //  look through current params and parse them to object instead of string
+            currentQuery = qs.parse(params.toString());
+        }
+
+        // spread current query and add label category
+        const updatedQuery: any = {
+            ...currentQuery,
+            category: label
+        }
+
+        /* check if the new the category is already selected and removed if from updated query if so to deselect it
+        like and on/off toggle*/
+        if (params?.get('category') === label) {
+            delete updatedQuery.category;
+        }
+
+        //  generate url string
+        const url = qs.stringifyUrl({
+            url: '/',
+            query: updatedQuery
+        }, { skipNull: true });
+
+        router.push(url);
+    }, [label, params, router]); // dependency array
+
     return (
         <div
+            onClick={handleClick}
             className={`
                 flex
                 flex-col
