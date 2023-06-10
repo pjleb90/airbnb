f4bc894007c3daebf2244ced60fce81e34b2ed59
pjleb90 <pjleb90@gmail.com>
2023-06-10 15:24:36 -0400

    created CategoryInput, added useForm FieldValues in RentModal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -4,9 +4,11 @@ import useRentModal from "@/app/hooks/useRentModal";
 
 import Modal from "./Modal";
 import Heading from "../Heading";
+import CategoryInput from "../inputs/CategoryInput";
 import { categories } from "../navbar/Categories";
 
 import { useState, useMemo } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 
 enum STEPS {
     CATEGORY = 0,
@@ -21,6 +23,39 @@ const RentModal = () => {
     const rentModal = useRentModal();
 
     const [step, setStep] = useState(STEPS.CATEGORY);
+
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        watch,
+        formState: {
+        errors,
+        },
+        reset,
+    } = useForm<FieldValues>({
+        defaultValues: {
+        category: '',
+        location: null,
+        guestCount: 1,
+        roomCount: 1,
+        bathroomCount: 1,
+        imageSrc: '',
+        price: 1,
+        title: '',
+        description: '',
+        }
+    });
+
+    const category = watch('category');
+
+    const setCustomValue = (id: string, value: any) => {
+        setValue(id, value, {
+            shouldValidate: true,
+            shouldDirty: true,
+            shouldTouch: true,
+        })
+    }
 
     const onBack = () => {
         setStep((value) => value - 1);
@@ -63,7 +98,13 @@ const RentModal = () => {
             >
                 {categories.map((item) => (
                     <div key={item.label} className="col-span-1">
-                        {item.label}
+                        <CategoryInput
+                            onClick={(category) =>
+                                setCustomValue('category', category)}
+                            selected={category === item.label}
+                            label={item.label}
+                            icon={item.icon}
+                        />
                     </div>
                 ))}
             </div>
