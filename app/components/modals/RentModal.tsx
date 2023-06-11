a84ce6719864b6f62a7430f2a7cc006d2ffd34b6
pjleb90 <pjleb90@gmail.com>
2023-06-11 09:06:15 -0400

    created useCountrires hook

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -5,6 +5,7 @@ import useRentModal from "@/app/hooks/useRentModal";
 import Modal from "./Modal";
 import Heading from "../Heading";
 import CategoryInput from "../inputs/CategoryInput";
+import CountrySelect from "../inputs/CountrySelect";
 import { categories } from "../navbar/Categories";
 
 import { useState, useMemo } from "react";
@@ -110,11 +111,25 @@ const RentModal = () => {
             </div>
         </div>
     )
+
+    if (step === STEPS.LOCATION) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                title="Where is your place located?"
+                subtitle="Help guests find you!"
+                />
+                <CountrySelect
+
+                />
+            </div>
+        )
+    }
     return (
         <Modal
             isOpen={rentModal.isOpen}
             onClose={rentModal.onClose}
-            onSubmit={rentModal.onClose}
+            onSubmit={onNext}
             actionLabel={actionLabel}
             secondaryActionLabel={secondaryActionLabel}
             secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
