e80e9a0bfec7746ac3c0ff9afec24c623d63a65a
pjleb90 <pjleb90@gmail.com>
2023-06-17 14:13:27 -0400

    added descpription and prices steps to the RentModal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,6 +9,8 @@ import Heading from "../Heading";
 import CategoryInput from "../inputs/CategoryInput";
 import CountrySelect from "../inputs/CountrySelect";
 import Counter from "../inputs/Counter";
+import Input from "../inputs/Input";
+
 import ImageUpload from "../inputs/ImageUpload";
 
 import { categories } from "../navbar/Categories";
@@ -16,6 +18,7 @@ import { categories } from "../navbar/Categories";
 import { useState, useMemo } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 import dynamic from "next/dynamic";
+
 
 
 
@@ -32,6 +35,7 @@ const RentModal = () => {
     const rentModal = useRentModal();
 
     const [step, setStep] = useState(STEPS.CATEGORY);
+    const [isLoading, setIsLoading] = useState(false);
 
     const {
         register,
@@ -131,67 +135,116 @@ const RentModal = () => {
 
     if (step === STEPS.LOCATION) {
         bodyContent = (
-        <div className="flex flex-col gap-8">
-            <Heading
-            title="Where is your place located?"
-            subtitle="Help guests find you!"
-            />
-            <CountrySelect
-            value={location}
-            onChange={(value) => setCustomValue('location', value)}
-            />
-            <div></div>
-            <div></div>
-            <Map center={location?.latlng} />
-        </div>
-        );
+            <div className="flex flex-col gap-8">
+                <Heading
+                title="Where is your place located?"
+                subtitle="Help guests find you!"
+                />
+                <CountrySelect
+                value={location}
+                onChange={(value) => setCustomValue('location', value)}
+                />
+                <div></div>
+                <div></div>
+                <Map center={location?.latlng} />
+            </div>
+        )
     }
 
-  if (step === STEPS.INFO) {
-    bodyContent = (
-      <div className="flex flex-col gap-8">
-        <Heading
-          title="Share some basics about your place"
-          subtitle="What amenitis do you have?"
-        />
-        <Counter
-          onChange={(value) => setCustomValue('guestCount', value)}
-          value={guestCount}
-          title="Guests"
-          subtitle="How many guests do you allow?"
-        />
-        <hr />
-        <Counter
-          onChange={(value) => setCustomValue('roomCount', value)}
-          value={roomCount}
-          title="Rooms"
-          subtitle="How many rooms do you have?"
-        />
-        <hr />
-        <Counter
-          onChange={(value) => setCustomValue('bathroomCount', value)}
-          value={bathroomCount}
-          title="Bathrooms"
-          subtitle="How many bathrooms do you have?"
-        />
-      </div>
-    )
-  }
-
-  if (step === STEPS.IMAGES) {
-    bodyContent = (
-        <div className="flex flex-col gap-8">
+    if (step === STEPS.INFO) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
             <Heading
-            title="Add a photo of your place"
-            subtitle="Show guests what your place looks like"
+                title="Share some basics about your place"
+                subtitle="What amenitis do you have?"
             />
-            <ImageUpload
-                onChange={(value) => setCustomValue('imageSrc', value)}
-                value={imageSrc}
+            <Counter
+                onChange={(value) => setCustomValue('guestCount', value)}
+                value={guestCount}
+                title="Guests"
+                subtitle="How many guests do you allow?"
             />
-        </div>
-    )
-  }
+            <hr />
+            <Counter
+                onChange={(value) => setCustomValue('roomCount', value)}
+                value={roomCount}
+                title="Rooms"
+                subtitle="How many rooms do you have?"
+            />
+            <hr />
+            <Counter
+                onChange={(value) => setCustomValue('bathroomCount', value)}
+                value={bathroomCount}
+                title="Bathrooms"
+                subtitle="How many bathrooms do you have?"
+            />
+            </div>
+        )
+    }
+
+    if (step === STEPS.IMAGES) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                title="Add a photo of your place"
+                subtitle="Show guests what your place looks like"
+                />
+                <ImageUpload
+                    onChange={(value) => setCustomValue('imageSrc', value)}
+                    value={imageSrc}
+                />
+            </div>
+        )
+    }
+
+    if (step === STEPS.DESCRIPTION) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+            <Heading
+                title="How would you describe your place?"
+                subtitle="Short and sweet works best!"
+            />
+            <Input
+                id="title"
+                label="Title"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+            <hr />
+            <Input
+                id="description"
+                label="Description"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+            </div>
+        )
+    }
+
+    if (step === STEPS.PRICE) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+            <Heading
+                title="Now, set your price"
+                subtitle="How much do you charge per night?"
+            />
+            <Input
+                id="price"
+                label="Price"
+                formatPrice
+                type="number"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+            </div>
+        )
+    }
 
     return (
         <Modal
