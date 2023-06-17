37443006e2a191bd3237733a7fc6dc0b84c5b50f
pjleb90 <pjleb90@gmail.com>
2023-06-17 08:53:43 -0400

    added counters for rooms and bathrooms

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -58,6 +58,8 @@ const RentModal = () => {
     const category = watch('category');
     const location = watch('location');
     const guestCount = watch('guestCount');
+    const roomCount = watch('roomCount');
+    const bathroomCount = watch('bathroomCount');
 
     const Map = useMemo(() => dynamic(() => import('../Map'), {
         ssr: false
@@ -143,22 +145,36 @@ const RentModal = () => {
         );
     }
 
-    if (step === STEPS.INFO) {
-        bodyContent = (
-            <div className="flex flex-col gap-8">
-                <Heading
-                    title="Share some basics about your place"
-                    subtitle="What amenities do you have?"
-                />
-                <Counter
-                    title="Number of guests"
-                    subtitle="How many guests do you allow?"
-                    value={guestCount}
-                    onChange={(value) => setCustomValue('guestCount', value)}
-                />
-            </div>
-        )
-    }
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Share some basics about your place"
+          subtitle="What amenitis do you have?"
+        />
+        <Counter
+          onChange={(value) => setCustomValue('guestCount', value)}
+          value={guestCount}
+          title="Guests"
+          subtitle="How many guests do you allow?"
+        />
+        <hr />
+        <Counter
+          onChange={(value) => setCustomValue('roomCount', value)}
+          value={roomCount}
+          title="Rooms"
+          subtitle="How many rooms do you have?"
+        />
+        <hr />
+        <Counter
+          onChange={(value) => setCustomValue('bathroomCount', value)}
+          value={bathroomCount}
+          title="Bathrooms"
+          subtitle="How many bathrooms do you have?"
+        />
+      </div>
+    )
+  }
 
     return (
         <Modal
