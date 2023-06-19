5ff438fbcf9310da19697db475a63808bcf13653
pjleb90 <pjleb90@gmail.com>
2023-06-19 10:46:44 -0400

    replaced listing with SafeListing type to resolve coient component error

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -8,14 +8,14 @@ import { useCallback, useMemo } from "react";
 import { format } from 'date-fns';
 
 import useCountries from "@/app/hooks/useCountries";
-import { SafeUser } from "@/app/types";
+import { SafeUser, SafeListing } from "@/app/types";
 
 import HeartButton from "../HeartButton";
 import Button from "../Button";
 
 
 interface ListingCardProps {
-  data: Listing;
+  data: SafeListing;
   reservation?: Reservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
