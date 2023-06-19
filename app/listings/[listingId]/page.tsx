a5c5cda820c9b54d7de5c5fda5f3cf61bc086368
pjleb90 <pjleb90@gmail.com>
2023-06-19 16:10:40 -0400

    created components to display individual listing details page

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,6 +1,34 @@
-const ListingPage = () => {
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import getListingById from "@/app/actions/getListingById";
+
+import ClientOnly from "@/app/components/ClientOnly";
+import EmptyState from "@/app/components/EmptyState";
+
+import ListingClient from "./ListingClient";
+
+interface IParams {
+  listingId?: string;
+}
+
+const ListingPage = async ({ params }: { params: IParams }) => {
+
+  const listing = await getListingById(params);
+  const currentUser = await getCurrentUser();
+
+  if (!listing) {
     return (
-        <div>My listings</div>
+        <ClientOnly>
+            <EmptyState />
+        </ClientOnly>
+    )
+  }
+    return (
+        <ClientOnly>
+            <ListingClient
+                listing={listing}
+                currentUser={currentUser}
+            />
+        </ClientOnly>
       );
 }
 
