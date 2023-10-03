80753feb2e3a07f3f568c32bba7d2d6b720d0102
pjleb90 <pjleb90@gmail.com>
2023-10-03 19:10:35 -0400

    added a footer with legal disclaimer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+    return (
+        <footer className="bg-white border-t">
+            <div className="mx-auto py-10">
+                <p className="text-center text-xs text-black">
+                    Disclaimer:
+                        This website is a portfolio project created by PJ Le Blanc, a web developer based in Waterloo, Ontario, Canada. It is intended solely for demonstration and showcase purposes.
+                        This website is not affiliated with, endorsed by, or connected to Airbnb, Inc. It does not provide any real booking or accommodation services. Any resemblance to the design or functionality of the actual Airbnb website is purely coincidental.
+                        All content, including text, images, and code, on this website has been created for educational and demonstration purposes only. No actual bookings, transactions, or reservations can be made through this website.
+                        If you have any questions or concerns about this portfolio project, please feel free to contact pjleb90@gmail.com.
+                        Thank you for visiting and understanding that this is a creative and educational project designed to showcase the skills and abilities of the developer.
+
+                </p>
+            </div>
+        </footer>
+    )
+};
+
+export default Footer;
