dce7213b0d3ac95cccf5b24ffc7157abcf035e83
pjleb90 <pjleb90@gmail.com>
2023-11-13 18:49:19 -0500

    small footer change

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
             <div className="mx-auto py-10">
                 <p className="text-center text-xs text-black">
                     Disclaimer:
-                        This website is a portfolio project created by PJ Le Blanc, a web developer based in Ontario, Canada. It is intended solely for demonstration and showcase purposes.
+                        This website is a portfolio project created by PJ LeBlanc, a web developer based in Ontario, Canada. It is intended solely for demonstration and showcase purposes.
                         This website is not affiliated with, endorsed by, or connected to Airbnb, Inc. It does not provide any real booking or accommodation services. Any resemblance to the design or functionality of the actual Airbnb website is purely coincidental.
                         All content, including text, images, and code, on this website has been created for educational and demonstration purposes only. No actual bookings, transactions, or reservations can be made through this website.
                         If you have any questions or concerns about this portfolio project, please feel free to contact pjleb90@gmail.com.
